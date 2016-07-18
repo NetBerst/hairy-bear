7e45a8729b399aa5c351aef5ccb8346474247f83
Artem Surov <artemsurov@gmail.com>
2016-07-18 22:51:04 +0300

    Добавлена регистрация

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, login, authenticate
 from django.template.response import TemplateResponse
+from blog.forms  import UserForm
+
 # Create your views here.
 
 class PostsListView(ListView):
@@ -14,40 +16,42 @@ class PostsListView(ListView):
     def get_queryset(self):
         return Post.objects.order_by('-datetime')
 
-    def get_context_data(self,**kwargs):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['authForm']= AuthenticationForm()
+        context['authForm'] = AuthenticationForm()
         return context
 
 
-def post_detail_func_view(request,pk):
-    #Получа объект или ошибку 404
-    postForComment = get_object_or_404(Post,id = pk)
+def post_detail_func_view(request, pk):
+    # Получа объект или ошибку 404
+    postForComment = get_object_or_404(Post, id=pk)
     context = {}
     context['post'] = postForComment
-    context['comment_list'] = Comment.objects.filter(post = pk)
+    context['comment_list'] = Comment.objects.filter(post=pk)
     template_name = 'blog/post_detail.html'
 
     if not request.user.is_authenticated():
         context['authForm'] = AuthenticationForm()
 
-    if  request.method == 'POST':
+    if request.method == 'POST':
 
         cf = CommentForm(request.POST)
-        context['form']= cf
+        context['form'] = cf
 
         if cf.is_valid():
             new_cf = cf.save(commit=False)
             new_cf.post = postForComment
             new_cf.save()
-            return HttpResponseRedirect(reverse('blog:detail',args=pk))
+            return HttpResponseRedirect(reverse('blog:detail', args=pk))
     else:
-        context['form']=CommentForm()
+        context['form'] = CommentForm()
 
-    return render(request,template_name,context)
+    return render(request, template_name, context)
+
 
 def list_of_page(request):
-    return TemplateResponse(request,"blog/index.html",{})
+    return TemplateResponse(request, "blog/index.html", {})
+
 
 def login_page(request):
     username = request.POST.get('username')
@@ -61,10 +65,30 @@ def login_page(request):
         else:
             return HttpResponse("Your account is disabled.")
     else:
-        #print "Invalid login details: {0}, {1}".format(username, password)
+        # print "Invalid login details: {0}, {1}".format(username, password)
         return HttpResponse("Invalid login details supplied.")
+
 
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect(reverse('blog:list'))
 
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+
+    else:
+        user_form = UserForm()
+
+    return render(request,
+                  'blog/register.html',
+                  {'user_form': user_form, 'registered': registered})
